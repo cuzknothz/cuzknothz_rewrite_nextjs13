4dbc8cad2e81a791871c18fe8be1844ff1d0830d
wize-nguyen.cuong <nguyen.cuong@wize-net.co.jp>
2022-11-22 23:22:32 +0700

    _

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useRef, useState } from "react";
 
-function Box(props) {
+function Box(props: any) {
   // This reference will give us direct access to the mesh
   const mesh = useRef();
   // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
+  useFrame((state, delta) => (mesh.current!.rotation.x += 0.01));
   // Return view, these are regular three.js elements expressed in JSX
   return (
     <mesh
